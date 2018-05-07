2914b54692c8424413ab857bcc458b7f06a77daf
Sean Clifford <sclifford6262@gmail.com>
2018-05-07 17:11:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ function driversByRevenue(drivers){
 function driversByName(drivers){
   const copyOfDrivers = [...drivers]
   return copyOfDrivers.sort(function(a,b){
-    if(a.name > b.name){return 1};
-    else if(a.name < b.name){return -1};
-    else {return 0};
+    if(a.name > b.name){
+      return 1
+    } else if(a.name < b.name){
+      return -1
+    } else {return 0};
   })
 }
